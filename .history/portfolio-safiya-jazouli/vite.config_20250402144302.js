250ba6f9c792fcdc523eb9cad1478ec73adc9e70
Safiya Jazouli <safiya.jazouli@epitech.eu>
2025-04-02 15:30:38 +0200

    Delete .history/portfolio-safiya-jazouli directory

--- .history/portfolio-safiya-jazouli/vite.config_20250402144302.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/W-WEB-090-LYN-1-1-Portfolio-safiya.jazouli/",
-});
